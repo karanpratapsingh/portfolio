a17a999ca7b41504b5b335cd738e4614e9bf5e4b
Karan Pratap Singh <synester.gates4@gmail.com>
2020-10-04 01:20:39 +0530

    added navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './src/navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App(): React.ReactElement {
+  return <AppNavigator />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
