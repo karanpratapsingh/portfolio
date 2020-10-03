aff07dc6709ac36d174c6da1e93d750ef4ca7a48
Karan Pratap Singh <synester.gates4@gmail.com>
2020-10-04 01:45:36 +0530

    project refactor

--- src/screens/Home/Projects.tsx
+++ src/screens/Home/Projects.tsx
@@ -7,42 +7,33 @@ import { useDimensions } from '../../hooks';
 import { ScreenType } from '../../navigation';
 import { useTheme } from '../../store';
 import { Colors, Theme, typography } from '../../theme';
-
-interface Project {
-  title: string;
-  banner: string;
-}
-
-const projects: Project[] = [
-  { title: 'Project A', banner: 'https://images.unsplash.com/photo-1601666703585-964591b026c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80' },
-  { title: 'Project B', banner: 'https://images.unsplash.com/photo-1601666621148-7100d0f63ff9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80' },
-  { title: 'Project C', banner: 'https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80' },
-  { title: 'Project D', banner: 'https://images.unsplash.com/photo-1601664023050-82d1e19d945c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80' },
-  { title: 'Project E', banner: 'https://images.unsplash.com/photo-1601658797866-7ae55d05a817?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80' },
-  { title: 'Project F', banner: 'https://images.unsplash.com/photo-1601658480525-0d3cf704ca48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80' },
-  { title: 'Project G', banner: 'https://images.unsplash.com/photo-1601648904883-4270e74277d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80' },
-];
+import { BlurView } from 'expo-blur';
+import Config, { IProject } from '../../config';
 
 function Projects(): React.ReactElement {
   const theme = useTheme(state => state.theme);
   const navigation = useNavigation();
   const { width } = useDimensions();
+  const { projects } = Config;
 
   const itemDimension = width <= 1000 ? width * 0.9 : width * 0.3;
 
   const ListHeaderComponent = () => <Text style={styles(theme).header}>Work</Text>
 
-  const renderItem = (info: ListRenderItemInfo<Project>): JSX.Element => {
-    const { title, banner } = info.item;
+  const renderItem = (info: ListRenderItemInfo<IProject>): JSX.Element => {
+    const { item: project } = info;
+    const { title, banner } = project;
 
     const onProject = () => {
-      navigation.navigate(ScreenType.Project);
+      navigation.navigate(ScreenType.Project, { project });
     };
 
     return (
       <TouchableOpacity onPress={onProject} activeOpacity={0.95} style={styles(theme).card}>
         <ImageBackground source={{ uri: banner }} style={styles(theme).banner} borderRadius={10}>
-          <Text style={styles(theme).title}>{title}</Text>
+          <BlurView style={styles(theme).cardContent}>
+            <Text style={styles(theme).title}>{title}</Text>
+          </BlurView>
         </ImageBackground>
       </TouchableOpacity>
     );
@@ -51,10 +42,10 @@ function Projects(): React.ReactElement {
   return (
     <FlatGrid
       data={projects}
+      spacing={10}
       ListHeaderComponent={ListHeaderComponent}
       itemDimension={itemDimension}
       style={styles(theme).gridView}
-      spacing={10}
       renderItem={renderItem}
     />
   );
@@ -82,7 +73,10 @@ const styles = (theme: Theme) => StyleSheet.create({
   banner: {
     height: 300,
     justifyContent: 'flex-end',
-    padding: 10,
+  },
+  cardContent: {
+    paddingVertical: 25,
+    paddingHorizontal: 10,
   },
   title: {
     fontSize: typography.subtitle,
--- src/screens/Project/index.tsx
+++ src/screens/Project/index.tsx
@@ -1,14 +1,24 @@
+import { RouteProp, useRoute } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { responsiveWidth } from 'react-native-responsive-dimensions';
+import { IProject } from '../../config';
 import { useTheme } from '../../store';
-import { Theme } from '../../theme';
+import { Colors, Theme, typography } from '../../theme';
+
+type ProjectParams = {
+  project: IProject;
+};
+
+type ProjectRouteParams = RouteProp<Record<string, ProjectParams | undefined>, string>;
 
 function Project(): React.ReactElement {
   const theme = useTheme(state => state.theme);
+  const { params } = useRoute<ProjectRouteParams>();
 
   return (
     <View style={styles(theme).container}>
+      <Text style={styles(theme).text}>{params?.project.title}</Text>
     </View>
   );
 }
@@ -19,6 +29,10 @@ const styles = (theme: Theme) => StyleSheet.create({
     paddingVertical: responsiveWidth(2),
     paddingHorizontal: responsiveWidth(2.5),
     backgroundColor: theme.primary
+  },
+  text: {
+    color: Colors.white,
+    fontSize: typography.body
   }
 });
 
