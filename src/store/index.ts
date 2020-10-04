460bd07e199dcb3c65f82e9c26ec6ca83745ba9a
Karan Pratap Singh <synester.gates4@gmail.com>
2020-10-04 17:27:14 +0530

    updated color scheme

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,17 @@
 import create, { SetState } from 'zustand';
 import { Theme, ThemeMap, ThemeVariant } from '../theme';
+import { Appearance } from 'react-native-appearance';
 
 type ThemeStore = {
   theme: Theme;
   setTheme: (type: ThemeVariant) => void;
 };
 
+const isDarkMode = Appearance.getColorScheme() === 'dark';
+const defaultVariant = isDarkMode ? ThemeVariant.DARK : ThemeVariant.LIGHT;
+
 const useTheme = create<ThemeStore>((set: SetState<ThemeStore>) => ({
-  theme: ThemeMap[ThemeVariant.LIGHT],
+  theme: ThemeMap[defaultVariant],
   setTheme: (type: ThemeVariant): void => set({ theme: ThemeMap[type] })
 }))
 
