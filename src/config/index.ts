93375176e3c26cd8a760ad2bb88b634726bfa7fa
Karan Pratap Singh <synester.gates4@gmail.com>
2020-11-30 22:53:23 +0530

    feat(config): turn off available for hire :tada:

--- src/config/index.ts
+++ src/config/index.ts
@@ -21,7 +21,7 @@ const Config: IConfig = {
   about: 'A software engineer who loves to collaborate and build well-structured, scalable applications. I’ve written code in high-paced and challenging environment with an emphasis on using best practices to develop high-quality software that meets project requirements, budget, and schedule. Also, an avid open source contributor who values learning and growing with people, teams, and technologies.',
   resume: 'https://drive.google.com/file/d/1f8R2yyvc6G-zGNJaW8l5TBC5Cfgipo3u/view?usp=sharing',
   renderArticles: true,
-  availableForHire: true,
+  availableForHire: false,
   devToUsername: 'karanpratapsingh',
   contact,
   projects
