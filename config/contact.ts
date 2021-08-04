186529538127318c3b2489665dd1c559c5d9694a
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-05 02:18:22 +0530

    config

---
+++ config/contact.ts
@@ -0,0 +1,17 @@
+export enum ContactType {
+  email = 'email',
+  twitter = 'twitter',
+  linkedIn = 'linkedIn',
+  github = 'github'
+}
+
+export type Contact = Record<ContactType, string>;
+
+const contact = {
+  email: 'contact@karanpratapsingh.com',
+  twitter: 'twitter.com/karan_6864',
+  linkedIn: 'linkedin.com/in/karanpratapsingh4999',
+  github: 'github.com/karanpratapsingh',
+};
+
+export default contact;
