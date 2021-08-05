6f9c9ca4bf49da30d40685d0e3d378826d7772c5
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-05 23:23:10 +0530

    refactor: use sass and swipter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import 'tailwindcss/tailwind.css';
-import '../styles/globals.css';
+import '../styles/globals.scss';
+import '../styles/list.scss';
 import type { AppProps } from 'next/app';
+
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
