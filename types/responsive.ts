8b1f118de43a60b463451a8ce2b1f4fc2c424102
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-06 03:02:58 +0530

    feat: add resp types

---
+++ types/responsive.ts
@@ -0,0 +1,17 @@
+export const BreakpointValues: Record<Breakpoint, number> = {
+  XS: 0,
+  SM: 640,
+  MD: 768,
+  LG: 1024,
+  XL: 1280,
+  XXL: 1536,
+};
+
+export enum Breakpoint {
+  XS = 'XS',
+  SM = 'SM',
+  MD = 'MD',
+  LG = 'LG',
+  XL = 'XL',
+  XXL = 'XXL',
+}
