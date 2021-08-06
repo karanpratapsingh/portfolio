abb9073b53560ca42cd21f25664696f79021c011
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-06 13:25:42 +0530

    minor: fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { Layout } from 'antd';
 function Footer(): React.ReactElement {
   const year: number = new Date().getFullYear();
   return (
-    <Layout.Footer className='text-center py-10 text-lg font-light'>
+    <Layout.Footer className='text-center py-10 font-light bg-secondary'>
       Copyright © {year} Karan Pratap Singh | All rights reserved
     </Layout.Footer>
   );
--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,4 @@ export * from './Banner';
 export * from './Footer';
 export * from './List';
 export * from './Card';
+export * from './Header';
