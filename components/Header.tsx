ded300c6ab7ab959c5a1eb8b05ea0dacba9b8611
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-06 13:25:26 +0530

    feat: header

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import { PageHeader, Switch } from 'antd';
+import Image from 'next/image';
+
+function Header(): React.ReactElement {
+  const dimensions = { height: 20, width: 20 };
+
+  const checked = <Image src='/assets/svg/switch/sun.svg' {...dimensions} alt='sun-icon' />
+  const unchecked = <Image src='/assets/svg/switch/moon.svg' {...dimensions} alt='moon-icon' />
+
+  return (
+    <PageHeader
+      extra={
+        <div className='flex items-center'>
+          {checked}
+          <Switch className='mx-2' />
+          {unchecked}
+        </div>
+      }
+    />
+  );
+}
+
+export { Header }
