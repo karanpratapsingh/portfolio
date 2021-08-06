8e7086f369708b259515c1e7ff49086ee09652be
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-06 15:27:56 +0530

    refactor: types

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,12 @@
-export * from './responsive';
+export type Article = {
+  id: number,
+  title: string,
+  description: string,
+  url: string,
+  tags: string[],
+  publishedAt: string,
+};
+
+export type Video = {
+  id: string;
+};
