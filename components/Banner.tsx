340dcc07a9ed5bfef9fd18ac0251554e09e57c8f
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-06 15:27:51 +0530

    refactor: config

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,8 +3,8 @@ import config from '../config';
 function Banner(): React.ReactElement {
   return (
     <div className='h-screen flex flex-col flex-1 justify-center px-6 lg:px-10  py-10' >
-      <h1 className='text-2xl lg:text-6xl font-bold'>Hi, I am {config.name}</h1>
-      <p className='mt-2 lg:text-2xl font-light'>{config.intro}</p>
+      <h1 className='text-2xl lg:text-6xl font-bold'>Hi, I am {config.personal.name}</h1>
+      <p className='mt-2 lg:text-2xl font-light'>{config.personal.intro}</p>
       <p className='mt-2 lg:text-2xl font-light'>
         Read more
         <span className='cursor-pointer mx-2 font-bold underline italic'>
--- config/index.ts
+++ config/index.ts
@@ -1,36 +1,65 @@
 import contact, { Contact } from './contact';
 import projects, { Project } from './projects';
 
-interface Config {
+type PersonalConfig = {
   name: string;
   fullName: string;
   intro: string;
   about: string;
   resume: string;
   contact: Contact;
-  availableForHire: boolean;
-  renderArticles: boolean;
-  devToUsername: string;
+  available: boolean;
+};
+
+type ArticleProvider = 'dev.to';
+
+type ArticlesConfig = {
+  provider: ArticleProvider;
+  username: string;
+  apiUrl: string;
+};
+
+type VideosProvider = 'youtube.com';
+
+type VideosConfig = {
+  provider: VideosProvider;
+  apiUrl: string;
+};
+
+interface Config {
+  personal: PersonalConfig;
+  articles?: ArticlesConfig;
+  videos?: VideosConfig;
   projects: Project[];
 }
 
 const config: Config = {
-  name: 'Karan',
-  fullName: 'Karan Pratap Singh',
-  intro:
-    'A full stack developer who loves open source and values learning and growing with people, teams, and technologies.',
-  about:
-    'A software engineer who loves to collaborate and build well-structured, scalable applications. I’ve written code in high-paced and challenging environment with an emphasis on using best practices to develop high-quality software that meets project requirements, budget, and schedule. Also, an avid open source contributor who values learning and growing with people, teams, and technologies.',
-  resume:
-    'https://drive.google.com/file/d/1f8R2yyvc6G-zGNJaW8l5TBC5Cfgipo3u/view?usp=sharing',
-  renderArticles: true,
-  availableForHire: false,
-  devToUsername: 'karanpratapsingh',
-  contact,
+  personal: {
+    name: 'Karan',
+    fullName: 'Karan Pratap Singh',
+    intro:
+      'A full stack developer who loves open source and values learning and growing with people, teams, and technologies.',
+    about:
+      'A software engineer who loves to collaborate and build well-structured, scalable applications. I’ve written code in high-paced and challenging environment with an emphasis on using best practices to develop high-quality software that meets project requirements, budget, and schedule. Also, an avid open source contributor who values learning and growing with people, teams, and technologies.',
+    resume:
+      'https://drive.google.com/file/d/1f8R2yyvc6G-zGNJaW8l5TBC5Cfgipo3u/view?usp=sharing',
+    available: false,
+    contact,
+  },
   projects,
+  articles: {
+    provider: 'dev.to',
+    username: 'karanpratapsingh',
+    apiUrl: 'https://dev.to/api',
+  },
+  videos: {
+    provider: 'youtube.com',
+    apiUrl: 'https://www.googleapis.com/youtube/v3',
+  },
 };
 
 export default config;
+
 export * from './projects';
 export * from './contact';
 export * from './stack';
