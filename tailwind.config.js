52592f6f27f9a852fac994d5044fd40552bbb835
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-06 18:29:24 +0530

    update: primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#fcf5ff'
+      }
+    },
   },
   variants: {
     extend: {},
