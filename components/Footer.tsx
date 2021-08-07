5eb00393d9b908136445b306e4bab4c5cd3e49c6
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-08 00:32:27 +0530

    fix: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,13 +11,18 @@ import { Colors, contact, ContactType } from '../config';
 import { Maybe } from '../types';
 
 export function Footer(): React.ReactElement {
+  const year: number = new Date().getFullYear();
+
   return (
-    <Layout.Footer className='flex flex-col items-center py-10 lg:py-16 font-light bg-primary'>
+    <Layout.Footer className='flex flex-col items-center py-10 lg:py-12 font-light bg-primary'>
       <span className='text-xl font-bold'>Let&apos;s connect</span>
-      <span className='mt-4 lg:mt-8 lg:mb-6 font-light'>
-        Get in touch for opportunities or just to say hi!
+      <span className='mt-4 mb-2 lg:text-base lg:mt-6 lg:mb-4 font-light'>
+        Get in touch for opportunities or to say hi!
       </span>
       <SocialIcons />
+      <span className='mt-12 lg:mt-16 text-xs font-light'>
+        &copy; {year} Karan Pratap Singh
+      </span>
     </Layout.Footer>
   );
 }
