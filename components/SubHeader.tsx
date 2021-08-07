f12fb12a0588b8577bed0c420bb801df51900a5e
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-07 16:37:42 +0530

    update: subheader

--- components/SubHeader.tsx
+++ components/SubHeader.tsx
@@ -1,15 +1,18 @@
 export interface SubHeaderProps {
-  title: string;
-  description: string;
+  title: React.ReactNode;
+  description: React.ReactNode;
+  className?: string;
+  children?: React.ReactNode;
 }
 
 export function SubHeader(props: SubHeaderProps): React.ReactElement {
-  const { title, description } = props;
+  const { title, description, className, children } = props;
 
   return (
-    <div className='px-6 lg:px-10 pb-4'>
-      <p className='text-2xl lg:text-2xl font-bold'>{title}</p>
-      <p className='mt-1 text-base font-light'>{description}</p>
+    <div className={`px-6 lg:px-10 pb-4 ${className}`}>
+      <h2 className='text-2xl lg:text-2xl font-bold mb-1'>{title}</h2>
+      <p className='text-base font-light'>{description}</p>
+      {children}
     </div>
   );
 }
