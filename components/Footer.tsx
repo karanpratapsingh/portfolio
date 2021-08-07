9b2936370a62b8ce77eef046d93c752e47787aef
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-07 17:18:55 +0530

    refactor: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,53 +11,57 @@ import { contact, ContactType } from '../config';
 import { Maybe } from '../types';
 
 export function Footer(): React.ReactElement {
-  function renderSocialIcons(): React.ReactNode {
-    function resolveIcon(entry: [string, string]): React.ReactNode {
-      const [type, url] = entry;
-
-      const props: IconBaseProps = {
-        className: 'cursor-pointer text-2xl mr-4',
-      };
-
-      let icon: Maybe<React.ReactNode> = null;
-
-      switch (type) {
-        case ContactType.linkedin:
-          icon = <LinkedinIcon {...props} />;
-          break;
-
-        case ContactType.twitter:
-          icon = <TwitterIcon {...props} />;
-          break;
-
-        case ContactType.github:
-          icon = <GithubIcon {...props} />;
-          break;
-
-        case ContactType.email:
-          icon = <MailIcon {...props} />;
-          break;
-        default:
-          break;
-      }
-
-      return (
-        <a target='_blank' href={url} rel='noopener noreferrer'>
-          {icon}
-        </a>
-      );
-    }
-
-    return React.Children.toArray(Object.entries(contact).map(resolveIcon));
-  }
-
   return (
     <Layout.Footer className='flex flex-col text-center py-10 lg:py-16 font-light bg-primary'>
       <span className='text-xl font-bold'>Let&apos;s connect</span>
-      <span className='my-4 lg:my-8 font-light'>
+      <span className='mt-4 lg:mt-8 lg:mb-6 font-light'>
         Get in touch for opportunities or just to say hi!
       </span>
-      <div className='flex self-center'>{renderSocialIcons()}</div>
+      <SocialIcons />
     </Layout.Footer>
+  );
+}
+
+export function SocialIcons(): React.ReactElement {
+  return (
+    <div className='flex self-center mt-2'>
+      {React.Children.toArray(Object.entries(contact).map(resolveIcon))}
+    </div>
+  );
+}
+
+function resolveIcon(entry: [string, string]): React.ReactNode {
+  const [type, url] = entry;
+
+  const props: IconBaseProps = {
+    className: 'cursor-pointer text-2xl mr-4',
+  };
+
+  let icon: Maybe<React.ReactNode> = null;
+
+  switch (type) {
+    case ContactType.linkedin:
+      icon = <LinkedinIcon {...props} />;
+      break;
+
+    case ContactType.twitter:
+      icon = <TwitterIcon {...props} />;
+      break;
+
+    case ContactType.github:
+      icon = <GithubIcon {...props} />;
+      break;
+
+    case ContactType.email:
+      icon = <MailIcon {...props} />;
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <a target='_blank' href={url} rel='noopener noreferrer'>
+      {icon}
+    </a>
   );
 }
