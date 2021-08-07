6fb236ebcbce37c2717c5f993aee588d70a83bcc
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-07 13:44:09 +0530

    feat: randomize colors

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,22 +1,43 @@
+import { useMemo } from 'react';
 import config from '../config';
 
-export function Banner(): React.ReactElement {
+interface BannerProps {
+  onClick: () => void
+}
+
+export function Banner(props: BannerProps): React.ReactElement {
+  const { onClick } = props;
+
+  const colors = useMemo(() => randomizeColor(), []);
+
   return (
     <div className='banner flex flex-col flex-1 justify-center px-6 lg:px-10 py-10'>
-      <h1 className='text-2xl lg:text-5xl font-bold'>
+      <h1 className='text-3xl lg:text-5xl font-bold'>
         Hi, I am {config.personal.name}
       </h1>
-      <p className='my-4 lg:text-2xl font-light'>{config.personal.title}</p>
+      <p className='my-2 text-lg lg:my-4 lg:text-2xl font-light'>{config.personal.title}</p>
       <p className='lg:text-xl font-light'>
         Read more
-        <span className='cursor-pointer mx-2 font-bold underline italic'>
+        <span onClick={onClick} className='cursor-pointer mx-2 font-medium p-1' style={{ backgroundColor: colors[0] }}>
           about me
         </span>
         or
-        <span className='cursor-pointer mx-2 font-bold underline italic'>
+        <span className='cursor-pointer mx-2 font-medium p-1' style={{ backgroundColor: colors[1] }}>
           contact me
         </span>
       </p>
     </div>
   );
+}
+
+function randomizeColor(): string[] {
+  const colors: string[][] = [
+    ['#F5E1FF', '#FFF8BC'],
+    ['#caf0f8', '#C9f2c7'],
+    ['#d8e2dc', '#faedcd'],
+    ['#eeebff', '#f7d9c4'],
+  ];
+
+  const random: number = Math.round(Math.random() * (colors.length - 1));
+  return colors[random];
 }
