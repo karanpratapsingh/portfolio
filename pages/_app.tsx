56036238997f60991d1791d1d18107c77a830314
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-20 01:38:29 +0530

    feat: absolute import styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.scss';
-import '../styles/bottomsheet.scss';
-import '../styles/list.scss';
+import 'styles/globals.scss';
+import 'styles/bottomsheet.scss';
+import 'styles/list.scss';
 
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
