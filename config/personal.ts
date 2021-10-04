f84ca330e2a102a34e6211c1fd1126afcdd19e65
Karan Pratap Singh <synester.gates4@gmail.com>
2021-10-05 01:05:59 +0530

    update: about

--- config/personal.ts
+++ config/personal.ts
@@ -12,11 +12,10 @@ export const personal: Personal = {
   fullName: 'Karan Pratap Singh',
   title: 'Software Engineer & Solutions Architect',
   about: `
-    A software engineer who loves to collaborate and build well-structured, scalable applications.
-    I’ve written code in high-paced and challenging environment with an emphasis on using best practices
-    to develop high-quality software that meets project requirements, budget, and schedule. Also, I am an
-    avid open source contributor who values learning and growing with people, teams, and technologies.
-  `,
+    Diligent software engineer who values learning and growing with people, teams, and technologies. Experienced at writing 
+    code in a high-paced environment with an emphasis on using best practices to develop high-quality software that satisfies 
+    project requirements, budget, and schedule.
+ `,
   resume:
     'https://drive.google.com/file/d/13ZAZlisvJ_F1Bo8q1e7KJDlkAAt6uNXZ/view?usp=sharing',
   available: false,
