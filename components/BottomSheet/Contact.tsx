2c22f57cdb5a121e06cd616e0e275c01d2fa6f09
Karan Pratap Singh <synester.gates4@gmail.com>
2022-02-16 00:31:56 +0530

    feat: integrate calendly

--- components/BottomSheet/Contact.tsx
+++ components/BottomSheet/Contact.tsx
@@ -1,4 +1,6 @@
+import { contact } from 'config/contact';
 import React, { memo } from 'react';
+import { openPopupWidget } from 'react-calendly';
 import SocialIcons from '../SocialIcons';
 import SubHeader from '../SubHeader';
 import BaseBottomSheet, { BaseBottomSheetProps } from './Base';
@@ -10,12 +12,35 @@ function ContactBottomSheet(
 ): React.ReactElement {
   const { open, onDismiss } = props;
 
+  function onScheduleMeeting(): void {
+    if (!contact.calendly) {
+      console.error('err: calendly link was not provided.');
+      return;
+    }
+
+    const config = {
+      url: contact.calendly,
+    };
+    openPopupWidget(config);
+    onDismiss();
+  }
+
+  const description: React.ReactNode = (
+    <span>
+      Do you have a project in mind? Want to hire me? or simply wanna chat? Feel
+      free to
+      <span
+        className='cursor-pointer underline ml-1'
+        onClick={onScheduleMeeting}
+      >
+        schedule a meeting.
+      </span>
+    </span>
+  );
+
   return (
     <BaseBottomSheet open={open} onDismiss={onDismiss}>
-      <SubHeader
-        title={`Let's Connect`}
-        description='Do you have a project in mind? Want to hire me? or simply wanna chat? Feel free to reach out'
-      >
+      <SubHeader title={`Let's Connect`} description={description}>
         <SocialIcons className='mt-4 mb-2' />
       </SubHeader>
     </BaseBottomSheet>
