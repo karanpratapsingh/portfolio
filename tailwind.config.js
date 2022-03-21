fbb47e3d7beb3b0e31f465f0c516d611e0acef9e
Karan Pratap Singh <synester.gates4@gmail.com>
2022-03-21 14:11:57 +0530

    fix: deps changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
