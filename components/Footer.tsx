60df205d5efa18c7d751e170109811fd2b62eac7
Karan Pratap Singh <synester.gates4@gmail.com>
2022-04-17 14:32:44 +0530

    fix: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer(): React.ReactElement {
   const year: number = new Date().getFullYear();
 
   return (
-    <div className='bg-primary dark:bg-primary-dark mt-4 flex flex-col items-center py-8 font-light dark:text-white lg:py-8'>
+    <div className='mt-4 flex flex-col items-center py-8 font-light dark:text-white lg:py-8'>
       <SocialIcons />
       <span className='mt-4 text-xs font-light lg:mt-8'>
         &copy; {year} Karan Pratap Singh
