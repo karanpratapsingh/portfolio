f6d001705e4d6def75b89f6c674b04440a3b4d0c
Karan Pratap Singh <synester.gates4@gmail.com>
2022-04-17 13:37:55 +0530

    font: open sans

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,24 @@
 import '@/css/global.css';
-import '@/css/tailwind.css';
 import '@/css/prism.css';
+import '@/css/tailwind.css';
 import 'katex/dist/katex.css';
 
-import '@fontsource/inter/variable-full.css';
 import '@fontsource/fira-code';
+import '@fontsource/open-sans/300.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/500.css';
+import '@fontsource/open-sans/600.css';
+import '@fontsource/open-sans/700.css';
+import '@fontsource/open-sans/800.css';
 
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import siteMetadata from '@/data/siteMetadata';
 import Analytics from '@/components/analytics';
-import LayoutWrapper from '@/components/LayoutWrapper';
 import { ClientReload } from '@/components/ClientReload';
+import LayoutWrapper from '@/components/LayoutWrapper';
+import siteMetadata from '@/data/siteMetadata';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 const isSocket = process.env.SOCKET;
