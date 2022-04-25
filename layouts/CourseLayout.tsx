90efc88488119d77e3ae0301086e4cab0a462cd5
Karan Pratap Singh <synester.gates4@gmail.com>
2022-04-25 18:00:35 +0530

    rm: date

--- layouts/CourseLayout.tsx
+++ layouts/CourseLayout.tsx
@@ -39,14 +39,14 @@ export default function CourseLayout({
   prev,
   children,
 }: Props) {
-  const { slug, fileName, date, title, readingTime, images } = frontMatter;
+  const { slug, fileName, title, readingTime, images } = frontMatter;
 
   const banner = images?.[0];
 
   return (
     <SectionContainer>
       <BlogSEO
-        url={`${siteMetadata.siteUrl}/blog/${slug}`}
+        url={`${siteMetadata.siteUrl}/courses/${slug}`}
         authorDetails={authorDetails}
         {...frontMatter}
       />
@@ -55,19 +55,6 @@ export default function CourseLayout({
         <div className='xl:divide-y xl:divide-gray-100 xl:dark:divide-gray-800'>
           <header className='pt-6 xl:pb-6'>
             <div className='space-y-1 text-center'>
-              <dl className='space-y-10'>
-                <div>
-                  <dt className='sr-only'>Published on</dt>
-                  <dd className='text-base font-medium leading-6 text-gray-500 dark:text-gray-400'>
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(
-                        siteMetadata.locale,
-                        postDateTemplate,
-                      )}
-                    </time>
-                  </dd>
-                </div>
-              </dl>
               <div>
                 <PageTitle>{title}</PageTitle>
               </div>
