da93e498b9443a2892290895e92f8f7b3cbf288e
Karan Pratap Singh <synester.gates4@gmail.com>
2022-04-25 14:24:00 +0530

    layouts

---
+++ layouts/CourseListLayout.tsx
@@ -0,0 +1,106 @@
+import { Header } from '@/components/Form';
+import Link from '@/components/Link';
+import Pagination from '@/components/Pagination';
+import formatDate from '@/lib/utils/formatDate';
+import { ComponentProps, useState } from 'react';
+import { PostFrontMatter } from 'types/PostFrontMatter';
+
+interface Props {
+  courses: PostFrontMatter[];
+  title: string;
+  initialDisplayPosts?: PostFrontMatter[];
+  pagination?: ComponentProps<typeof Pagination>;
+}
+
+export default function ListLayout({
+  courses,
+  title,
+  initialDisplayPosts = [],
+  pagination,
+}: Props) {
+  const [searchValue, setSearchValue] = useState('');
+  const filteredBlogPosts = courses.filter(frontMatter => {
+    const searchContent = frontMatter.title + frontMatter.summary;
+    return searchContent.toLowerCase().includes(searchValue.toLowerCase());
+  });
+
+  // If initialDisplayPosts exist, display it if no searchValue is specified
+  const displayPosts =
+    initialDisplayPosts.length > 0 && !searchValue
+      ? initialDisplayPosts
+      : filteredBlogPosts;
+
+  return (
+    <>
+      <div className='fade-in divide-y-2 divide-gray-100 dark:divide-gray-800'>
+        <Header title={title}>
+          <div className='relative max-w-lg'>
+            <input
+              aria-label='Search articles'
+              type='text'
+              onChange={({ target }) => setSearchValue(target.value)}
+              placeholder='Search articles'
+              className='block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100'
+            />
+            <svg
+              className='absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+              />
+            </svg>
+          </div>
+        </Header>
+
+        <ul>
+          {!filteredBlogPosts.length && (
+            <p className='mt-8 text-center'>No posts found</p>
+          )}
+          {displayPosts.map(frontMatter => {
+            const { slug, date, title, summary } = frontMatter;
+            return (
+              <li key={slug} className='py-4'>
+                <article className='space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0'>
+                  <dl>
+                    <dt className='sr-only'>Published on</dt>
+                    <dd className='text-base font-medium leading-6 text-gray-500 dark:text-gray-400'>
+                      <time dateTime={date}>{formatDate(date)}</time>
+                    </dd>
+                  </dl>
+                  <div className='space-y-3 xl:col-span-3'>
+                    <div>
+                      <h3 className='text-2xl font-bold leading-8 tracking-tight'>
+                        <Link
+                          href={`/courses/${slug}`}
+                          className='text-gray-900 dark:text-gray-100'
+                        >
+                          {title}
+                        </Link>
+                      </h3>
+                    </div>
+                    <div className='prose max-w-none text-gray-500 dark:text-gray-400'>
+                      {summary}
+                    </div>
+                  </div>
+                </article>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      {pagination && pagination.totalPages > 1 && !searchValue && (
+        <Pagination
+          currentPage={pagination.currentPage}
+          totalPages={pagination.totalPages}
+        />
+      )}
+    </>
+  );
+}
