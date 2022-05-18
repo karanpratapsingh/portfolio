beed421d1de8fbd47a3c9c79102a87a2b066b798
Karan Pratap Singh <synester.gates4@gmail.com>
2022-05-18 17:04:30 +0530

    fix: icons

--- components/Share.tsx
+++ components/Share.tsx
@@ -1,12 +1,10 @@
+import { Colors } from 'config/colors';
 import {
-  IoLogoFacebook as FacebookIcon,
-  IoLogoTwitter as TwitterIcon,
-} from 'react-icons/io';
-import {
-  SiGmail as EmailIcon,
-  SiLinkedin as LinkedinIcon,
-  SiReddit as RedditIcon,
-} from 'react-icons/si';
+  FaFacebook as FacebookIcon,
+  FaLinkedinIn as LinkedinIcon,
+} from 'react-icons/fa';
+import { IoLogoTwitter as TwitterIcon } from 'react-icons/io';
+import { SiGmail as EmailIcon, SiReddit as RedditIcon } from 'react-icons/si';
 import {
   EmailShareButton,
   FacebookShareButton,
@@ -14,7 +12,6 @@ import {
   RedditShareButton,
   TwitterShareButton,
 } from 'react-share';
-import { Colors } from 'config/colors';
 
 interface ShareProps {
   title: string;
@@ -28,16 +25,16 @@ export default function Share(props: ShareProps): React.ReactElement {
     <div className='flex items-center justify-center pt-2'>
       <div className='flex w-44 items-center justify-between'>
         <TwitterShareButton title={title} url={url}>
-          <TwitterIcon size={25} color={Colors.twitter} />
+          <TwitterIcon size={22} color={Colors.twitter} />
         </TwitterShareButton>
         <RedditShareButton title={title} url={url}>
           <RedditIcon size={22} color={Colors.reddit} />
         </RedditShareButton>
         <FacebookShareButton title={title} url={url}>
-          <FacebookIcon size={25} color={Colors.facebook} />
+          <FacebookIcon size={22} color={Colors.facebook} />
         </FacebookShareButton>
         <LinkedinShareButton title={title} url={url}>
-          <LinkedinIcon size={20} color={Colors.linkedin} />
+          <LinkedinIcon size={22} color={Colors.linkedin} />
         </LinkedinShareButton>
         <EmailShareButton title={title} url={url}>
           <EmailIcon size={22} color={Colors.email} />
